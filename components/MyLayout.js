4e2f56b8ef111993cf9b7c2269f65537a4924c4e
Kevin Wang <kwangsan@gmail.com>
2019-01-24 22:40:45 -0500

    Use Shared Components
    - add components/Header.js
    - add components/MyLayout.js
    - edit index.js & about.js

---
+++ components/MyLayout.js
@@ -0,0 +1,42 @@
+import Header from './Header'
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+}
+
+const Layout = (props) => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
+)
+
+export default Layout
+
+// other methods of creating a Layout component:
+
+/* 1.
+  import withLayout from '../lib/layout'
+
+  const Page = () => (
+    <p>This is the about page</p>
+)
+
+export default withLayout(Page)
+*/
+
+/* 2.
+  const Page = () => (
+    <p>This is the about page</p>
+  )
+
+  export default () => (<Layout page={Page}/>)
+*/
+
+/* 3.
+  const content = (<p>This is the about page</p>)
+
+  export default () => (<Layout content={content}/>)
+*/
