8ae3a9fac64b23637887de99252df1b2a83e02e3
Kevin Wang <kwangsan@gmail.com>
2019-03-04 22:09:38 -0500

    Prettier post.js

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,13 @@
-import Layout from '../components/MyLayout.js'
-import {withRouter} from 'next/router'
-import Markdown from 'react-markdown'
+import Layout from "../components/MyLayout.js";
+import { withRouter } from "next/router";
+import Markdown from "react-markdown";
 
-export default withRouter((props) => (
+export default withRouter(props => (
   <Layout>
-   <h1>{props.router.query.title}</h1>
-   <div className="markdown">
-     <Markdown source={`
+    <h1>{props.router.query.title}</h1>
+    <div className="markdown">
+      <Markdown
+        source={`
 This is our blog post.
 Yes. We can have a [link](/link).
 And we can have a title as well.
@@ -14,27 +15,28 @@ And we can have a title as well.
 ### This is a title
 
 And here's the content.
-     `}/>
-   </div>
-   <style jsx global>{`
-     .markdown {
-       font-family: 'Arial';
-     }
+     `}
+      />
+    </div>
+    <style jsx global>{`
+      .markdown {
+        font-family: "Arial";
+      }
 
-     .markdown a {
-       text-decoration: none;
-       color: blue;
-     }
+      .markdown a {
+        text-decoration: none;
+        color: blue;
+      }
 
-     .markdown a:hover {
-       opacity: 0.6;
-     }
+      .markdown a:hover {
+        opacity: 0.6;
+      }
 
-     .markdown h3 {
-       margin: 0;
-       padding: 0;
-       text-transform: uppercase;
-     }
-  `}</style>
+      .markdown h3 {
+        margin: 0;
+        padding: 0;
+        text-transform: uppercase;
+      }
+    `}</style>
   </Layout>
-))
+));
