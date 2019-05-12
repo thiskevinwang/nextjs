01737528c9a9f6b42065d84afe18ccac3d6a1098
Kevin Wang <kwangsan@gmail.com>
2019-05-12 15:18:34 -0400

    implement 'withLayout' HOC

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,19 @@
-import Layout from "../components/MyLayout.js";
+/** A */
+// import Layout from "components/MyLayout.js";
+//
+// export default () => (
+//   <Layout>
+//     <h1>About</h1>
+//     <p>Layout component</p>
+//   </Layout>
+// );
 
-export default () => (
-  <Layout>
+/** B */
+import withLayout from "lib/withLayout";
+
+export default withLayout(() => (
+  <>
     <h1>About</h1>
-    <p>This is the about page</p>
-  </Layout>
-);
+    <p>withLayout Higher Order Component</p>
+  </>
+));
